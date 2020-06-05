e147e123ef18a2a2a3d76bc31c3f61a8707052e5
YiminZhao <doctormin@sjtu.edu.cn>
2020-06-05 17:17:25 +0800

    added : kernel

---
+++ OperatingSystemPrj2/kernel/goldfish/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 3 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#3 PREEMPT Thu Jun 4 17:37:42 CST 2020"
+#define LINUX_COMPILE_BY "minozhao"
+#define LINUX_COMPILE_HOST "zym-linux"
+#define LINUX_COMPILER "gcc version 4.9 20150123 (prerelease) (GCC) "
