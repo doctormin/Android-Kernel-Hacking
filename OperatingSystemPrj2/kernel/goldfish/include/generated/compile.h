91d03c54be264ca4669f7527bd90423bcc8d5777
YiminZhao <doctormin@sjtu.edu.cn>
2020-06-18 17:34:25 +0800

    change to branch

--- OperatingSystemPrj2/kernel/goldfish/include/generated/compile.h
+++ OperatingSystemPrj2/kernel/goldfish/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 83 */
+/* This file is auto generated, version 84 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#83 PREEMPT Fri Jun 12 14:54:19 CST 2020"
+#define UTS_VERSION "#84 PREEMPT Tue Jun 16 16:17:19 CST 2020"
 #define LINUX_COMPILE_BY "minozhao"
 #define LINUX_COMPILE_HOST "zym-linux"
 #define LINUX_COMPILER "gcc version 4.9 20150123 (prerelease) (GCC) "
